67d32ada8c87ac1b64e910cd051ca16b05d5700b
Adshars <165464514+Adshars@users.noreply.github.com>
2025-12-07 12:58:24 +0100

    Organization model

---
+++ auth-service/src/models/Organization.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const organizationSchema = new mongoose.Schema({
+  // Organisation name
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  // Location data
+  country: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  city: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  address: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  // API Keys for machine access (B2B)
+  apiKey: {
+    type: String,
+    unique: true,
+    sparse: true // Allows for missing key before generation
+  },
+  // API Secret Hash
+  apiSecretHash: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model('Organization', organizationSchema);

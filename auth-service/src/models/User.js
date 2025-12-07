c48ed963e679e87d2729949859b5215f694599d6
Adshars <165464514+Adshars@users.noreply.github.com>
2025-12-07 15:18:08 +0100

    Model User

---
+++ auth-service/src/models/User.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+
+  // Relationship: User belongs to an organization
+  organizationId: {
+    type: mongoose.Schema.Types.ObjectId, // Organization ID in MongoDB
+    ref: 'Organization',
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+    // ID (._id) generuje się automatycznie.
+export default mongoose.model('User', userSchema);
